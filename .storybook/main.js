3588c235a05c92b5d021d066957337675477f903
hun2__2 <wognskec@hanyang.ac.kr>
2023-11-21 21:56:16 +0900

    [feat] Task 컴포넌트 생성
    1. CDD방법론으로 UI 개발
    2. 각 컴포넌트안에 **.js : **.stories.js = 1 : N 관계
      - **.js는 컴포넌트 개발
      - **.stories.js는 각 컴포넌트에 넣을 테스트 스토리를 넣을 수 있다.
    
    *주의! : react쓸꺼면 확장자 jsx로 해야된다.

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,10 +5,12 @@ const config = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    '@storybook/preset-create-react-app',
     "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
+    postcss: false,
     options: {},
   },
   docs: {
---
+++ src/components/Task.stories.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Task from "./Task.jsx";
+
+export default {
+  component: Task, // 타겟 컴포넌트
+  title: "Task", // 사이드바에 표시되는 스토리 분류명
+};
+
+// 컴포넌트를 반환하는 템플릿 함수를 만들어서 재사용함
+const Template = (args) => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: "1",
+    title: "Test Task",
+    state: "TASK_INBOX",
+    updatedAt: new Date(2021, 0, 1, 9, 0),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_PINNED",
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_ARCHIVED",
+  },
+};
